9318da33d80998f3746d1868c102870a9188ed1d
raphael-vinicius <raphaelvinu10@gmail.com>
2026-07-17 16:31:17 -0300

    Test home with hero only

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import { Hero } from "@/components/site/hero";
-import { Universe } from "@/components/site/universe";
 
 export default function HomePage() {
   return (
     <main id="conteudo">
       <Hero />
-      <Universe />
     </main>
   );
 }
